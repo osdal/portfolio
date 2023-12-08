cddfa5acff1c347044f92a7231b06bf2cb95b2d0
Дмитрий <osdal67@gmail.com>
2023-12-08 09:47:30 +0300

    start

---
+++ src/js/files/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // let buttonText = document.querySelector('header .button a');
+    // setInterval(function () {
+    //     // Генерация случайного цвета в формате #RRGGBB
+    //     var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    //     buttonText.style.color = randomColor;
+    // }, 5000); // Изменение цвета каждую секунду (1000 миллисекунд)
+});
+
+
+// Підключення функціоналу "Чертоги Фрілансера"
+import {
+    isMobile
+} from "./functions.js";
+// Підключення списку активних модулів
+import {
+    flsModules
+} from "./modules.js";
