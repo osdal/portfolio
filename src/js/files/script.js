d8980aaf80d0d889b08b51874667af70858e96ad
Дмитрий <osdal67@gmail.com>
2024-01-28 11:10:28 +0300

    Первая страница

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -5,6 +5,50 @@ document.addEventListener("DOMContentLoaded", function () {
     //     var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
     //     buttonText.style.color = randomColor;
     // }, 5000); // Изменение цвета каждую секунду (1000 миллисекунд)
+
+    function setActive() {
+        const menuItems = document.querySelectorAll('.leftsidebar__menu li');
+
+        menuItems.forEach(item => {
+            item.addEventListener('click', function () {
+                // Удаляем класс 'active' у всех пунктов меню
+                menuItems.forEach(menuItem => {
+                    menuItem.classList.remove('active');
+                });
+
+                // Присваиваем класс 'active' только кликнутому пункту
+                item.classList.add('active');
+            });
+        });
+    }
+
+    setActive();
+
+    var menuItems = document.querySelectorAll('.item-menu');
+
+        menuItems.forEach(function (item) {
+            item.addEventListener('click', function () {
+                // Проверяем наличие изображения в текущем элементе li
+                var existingImg = item.querySelector('img');
+                if (!existingImg) {
+                    // Удаляем img из всех элементов li
+                    menuItems.forEach(function (otherItem) {
+                        if (otherItem !== item) {
+                            var otherExistingImg = otherItem.querySelector('img');
+                            if (otherExistingImg) {
+                                otherExistingImg.remove();
+                            }
+                        }
+                    });
+
+                    // Добавляем img к текущему элементу li
+                    var img = document.createElement('img');
+                    img.src = 'img/bg_itemmenu_active.svg';
+                    // Можете добавить дополнительные атрибуты и стили, если необходимо
+                    item.appendChild(img);
+                }
+            });
+        });
 });
 
 
